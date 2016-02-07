d1abde85abb5c088acb3190a7efc6b8844ead0e8
zack <zack@squishymedia.com>
2016-02-06 19:40:25 -0800

    testing analytics

--- app/browser.js
+++ app/browser.js
@@ -3,6 +3,7 @@ import Router from 'react-router';
 import routes from './routes';
 
 Router.run(routes, (Handler, info) => {
+	console.log(info);
 	ga('set', 'page', info.path);
 	ga('send', 'pageview');
 	React.render(<Handler />, document.getElementById('app'));
