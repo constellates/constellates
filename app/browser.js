877f8d55cf121a6e3e2b6237cc1cc444b412d75a
zack <zack@squishymedia.com>
2016-02-06 19:38:56 -0800

    Added analytics

--- app/browser.js
+++ app/browser.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
 
-Router.run(routes, (Handler) => {
-  React.render(<Handler />, document.getElementById('app'));
+Router.run(routes, (Handler, info) => {
+	ga('set', 'page', info.path);
+	ga('send', 'pageview');
+	React.render(<Handler />, document.getElementById('app'));
 });
