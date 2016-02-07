455fb27d5a1bb124d3e4adc6f8a6419016ef4be6
zack <zack@squishymedia.com>
2016-02-06 19:43:20 -0800

    removed console loggging for analytics

--- app/browser.js
+++ app/browser.js
@@ -3,7 +3,6 @@ import Router from 'react-router';
 import routes from './routes';
 
 Router.run(routes, (Handler, info) => {
-	console.log(info);
 	ga('set', 'page', info.path);
 	ga('send', 'pageview');
 	React.render(<Handler />, document.getElementById('app'));
